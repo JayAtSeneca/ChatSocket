4407e2e8b3b571b4ff903efeae3df2255b2018e2
JayAtSeneca <jpchaudhari@myseneca.ca>
2023-05-14 23:31:46 -0400

    server.js: Created http server

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
 const app = express();
+const server = http.createServer(app);
+
+
 
 //Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
     }
 );
