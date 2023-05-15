6f9d4b463b166881ee41c4a7ffbda90d3122b248
JayAtSeneca <jpchaudhari@myseneca.ca>
2023-05-14 23:38:29 -0400

    server.js: update runs io server when client connects the server

--- server.js
+++ server.js
@@ -1,13 +1,34 @@
 const path = require('path');
 const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
+
 const app = express();
 const server = http.createServer(app);
+const io = socketio(server);
 
 
 
 //Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+//Run when client connects
+io.on('connection', socket => {
+    console.log('New WS Connection...');
+    socket.emit('message', 'Welcome to ChatCord!');
+    //Broadcast when a user connects
+    socket.broadcast.emit('message', 'A user has joined the chat');
+
+    //Runs when client disconnects
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left the chat');
+    });
+
+    //Listen for chatMessage
+    socket.on('chatMessage', msg => {
+        io.emit('message', msg);
+    });
+});
 
 const PORT = process.env.PORT || 3000;
 
