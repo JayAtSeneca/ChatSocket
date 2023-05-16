f2266c360d1fdc9ade021539ba61378821d33676
JayAtSeneca <jpchaudhari@myseneca.ca>
2023-05-16 13:02:33 -0400

    server.js: update added format message in socket channel

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-
+const botName = 'Bot';
 
 //Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
@@ -16,16 +17,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 io.on('connection', socket => {
     socket.emit('message', 'Welcome to ChatCord!');
     //Broadcast when a user connects
-    socket.broadcast.emit('message', 'A user has joined the chat');
+    socket.broadcast.emit('message', formatMessage(botName,'A user has joined the chat'));
 
     //Runs when client disconnects
     socket.on('disconnect', () => {
-        io.emit('message', 'A user has left the chat');
+        io.emit('message', formatMessage(botName, 'A user has left the chat'));
     });
 
     //Listen for chatMessage
     socket.on('chatMessage', msg => {
-        io.emit('message', msg);
+        io.emit('message', formatMessage('USER', msg));
     });
 });
 
