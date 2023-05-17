261c5c488d0e2f86bb01ce168b35e397a3d0b17c
JayAtSeneca <jpchaudhari@myseneca.ca>
2023-05-16 23:01:12 -0400

    server.js: update added join room socket event

--- server.js
+++ server.js
@@ -15,19 +15,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //Run when client connects
 io.on('connection', socket => {
-    socket.emit('message', 'Welcome to ChatCord!');
-    //Broadcast when a user connects
-    socket.broadcast.emit('message', formatMessage(botName,'A user has joined the chat'));
-
-    //Runs when client disconnects
-    socket.on('disconnect', () => {
-        io.emit('message', formatMessage(botName, 'A user has left the chat'));
+    socket.on('joinRoom', ({username, room})=>{
+        socket.emit('message', 'Welcome to ChatCord!');
+        //Broadcast when a user connects
+        socket.broadcast.emit('message', formatMessage(botName,'A user has joined the chat'));    
     });
 
     //Listen for chatMessage
     socket.on('chatMessage', msg => {
         io.emit('message', formatMessage('USER', msg));
     });
+
+    //Runs when client disconnects
+    socket.on('disconnect', () => {
+        io.emit('message', formatMessage(botName, 'A user has left the chat'));
+    }); 
 });
 
 const PORT = process.env.PORT || 3000;
